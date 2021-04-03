909b0217e031abe2aea1659b85af4ec07b6e2073
handesonic <2523490415@qq.com>
2021-04-03 09:12:09 +0800

    初步加工

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="tab-bar">
+      <slot></slot>
+      <!-- <div class="tab-bar-item">
+        <img src="../../assets/img/tabbar/home.svg" alt="">
+        首页
+        </div>
+      <div class="tab-bar-item">
+        <img src="../../assets/img/tabbar/home.svg" alt="">
+        分类
+        </div>
+      <div class="tab-bar-item">
+        <img src="../../assets/img/tabbar/home.svg" alt="">
+        购物车
+        </div>
+      <div class="tab-bar-item">
+        <img src="../../assets/img/tabbar/home.svg" alt="">
+        我的
+        </div> -->
+    </div>
+</template>
+
+<script>
+export default {
+  name: "TabBar"
+}
+</script>
+
+<style>
+
+  #tab-bar {
+    display: flex;
+    background-color: #f6f6f6;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+
+    box-shadow: 0 -1px 1px rgba(100,100,100,0.2);
+  }
+
+
+</style>
---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="recommend">
+    <div class="recommend-item" v-for="item in recommends" :key="item.index">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    recommends: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style>
+  .recommend {
+    display: flex;
+    text-align: center;
+    font-size: 14px;
+    padding: 10px 0;
+    border-bottom: 8px solid #ddd;
+  }
+  .recommend-item {
+    flex: 1;
+  }
+  .recommend-item img{
+    width: 80px;
+    height: 80px;
+  }
+</style>
